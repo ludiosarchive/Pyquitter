9ec3fb35d026c3d37589e7836107a897a9985c04
ivank <ivan@ludios.org>
2011-05-02 11:26:31 +0000

    Add setup.py and modsniffer/__init__.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+import modsniffer
+
+setup(
+	name='modsniffer',
+	version=modsniffer.__version__,
+	description="aka Imported Module Sniffer; detects file changes in imported modules",
+	packages=['modsniffer'],
+)
