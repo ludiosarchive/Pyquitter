f7fb6d50c9a9f39b956548afd58b2c1c3377769f
Ivan Kozik <ivan@ludios.org>
2011-05-14 08:34:36 +0000

    Change program name (not package name) from 'pyquitter' to 'Pyquitter'; add author and classifiers to setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,20 @@ from distutils.core import setup
 import pyquitter
 
 setup(
-	name='pyquitter',
+	name='Pyquitter',
 	version=pyquitter.__version__,
 	description="Detects file modifications to any imported module; "
 		"useful for auto-restarting during development.",
+	url="https://github.com/ludios/Pyquitter",
+	author="Ivan Kozik",
+	author_email="ivan@ludios.org",
+	classifiers=[
+		'Programming Language :: Python :: 2',
+		'Development Status :: 4 - Beta',
+		'Operating System :: OS Independent',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+	],
 	packages=['pyquitter'],
 	scripts=['bin/looper', 'bin/looper-stop'],
 )
